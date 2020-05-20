3d481d41f693ce1e35c1b07881263976bc808d24
Brittany Blake <brittany.blake49@gmail.com>
2020-05-20 12:38:37 +0200

    Add delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
   
     def index
         @articles = Article.all
@@ -13,11 +14,16 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new
-        @article.title = params[:article][:title]
-        @article.body = params[:article][:body]
+        @article = Article.new(article_params)
         @article.save
         redirect_to article_path(@article)
+    end
+
+    def destroy
+         @article = Article.find(params[:id])
+         @article.destroy
+         redirect_to @article
+
     end
 
 end
