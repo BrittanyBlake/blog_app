512c4d5cadb2f56de8a7ad524923951bdf4025fd
Brittany Blake <brittany.blake49@gmail.com>
2020-05-21 12:24:06 +0200

    fix linter errors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,16 +1,14 @@
 class AuthorsController < ApplicationController
-  before_action :set_author, only: [:show, :edit, :update, :destroy]
-  before_action :zero_authors_or_authenticated, only:[:new, :create]
-  before_action :require_login, except: [:new, :create]
-
+  before_action :set_author, only: %i[show edit update destroy]
+  before_action :zero_authors_or_authenticated, only: %i[new create]
+  before_action :require_login, except: %i[new create]
 
   def zero_authors_or_authenticated
     unless Author.count == 0 || current_user
       redirect_to root_path
-      return false
+      false
     end
   end
-
 
   # GET /authors
   # GET /authors.json
@@ -20,8 +18,7 @@ class AuthorsController < ApplicationController
 
   # GET /authors/1
   # GET /authors/1.json
-  def show
-  end
+  def show; end
 
   # GET /authors/new
   def new
@@ -29,8 +26,7 @@ class AuthorsController < ApplicationController
   end
 
   # GET /authors/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /authors
   # POST /authors.json
@@ -73,13 +69,14 @@ class AuthorsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_author
-      @author = Author.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def author_params
-      params.require(:author).permit(:username, :email, :password, :password_confirmation)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def author_params
+    params.require(:author).permit(:username, :email, :password, :password_confirmation)
+  end
 end
