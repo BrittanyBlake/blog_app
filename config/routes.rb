381691ecbe5050f6b3d1a60a05034b1fdac0e5a0
Brittany Blake <brittany.blake49@gmail.com>
2020-05-21 11:38:20 +0200

    Add login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
   end
   resources :tags
   resources :authors
+
+  resources :author_sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
+
 end
